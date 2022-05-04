fe05fa9a91aa75f41dc50bba17784e5de60bbba8
Jinny268 <101381590+Jinny268@users.noreply.github.com>
2022-05-04 11:24:59 +0900

    Update 06-01.py

--- 06-01.py
+++ 06-01.py
@@ -1,16 +1,16 @@
-#반복문 수행 유무 플래그
+# 반복문 수행 유무 플래그
 repeat=True
 
-#repeat이 True인 경우 whlie문 블록 수행
+# repeat이 True인 경우 whlie문 블록 수행
 while repeat:
 
-    #값 입력
+    # 값 입력
     flag = input('마음에 드는 옷을 찾았나요? (예/아니요): ')
 
-    #조건 분기
+    # 조건 분기
     if flag == '예':
         print(":) 축하합니다!!")
-        repeat = False
+        repeat = False   # repeat가 False가 되었으므로 반복 종료
 
     else:
         print(':( 아쉽군요')
