75b022e06e6ddee6bc4370e60ccbfdc3bb3157d1
Jinny268 <101381590+Jinny268@users.noreply.github.com>
2022-05-06 02:28:11 +0900

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,8 +1,9 @@
+# 홀수, 짝수 판별
 num=int(input('Please enter a number: '))
 
-if num==0:
+if num==0:  # 0은 홀수 짝수 둘 다 아님
     print('It is neither odd nor even.')
-elif num%2==1:
+elif num%2==1:   # 2로 나누었을 때 나머지가 1이면 홀수
     print('Odd number!')
-elif num%2==0:
+elif num%2==0:   # 2로 나누었을 때 나머지가 0이면 짝수
     print('Even number!')
