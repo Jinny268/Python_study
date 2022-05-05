e3b434d0026d21df29c131a99ac4d55a0b74d0f3
Jinny268 <101381590+Jinny268@users.noreply.github.com>
2022-05-06 02:25:35 +0900

    Add files via upload

---
+++ 34.py
@@ -0,0 +1,8 @@
+num=int(input('Please enter a number: '))
+
+if num==0:
+    print('It is neither odd nor even.')
+elif num%2==1:
+    print('Odd number!')
+elif num%2==0:
+    print('Even number!')
