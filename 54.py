eed4ec7419ff097d91dab52a401088b486796a00
Jinny268 <101381590+Jinny268@users.noreply.github.com>
2022-05-15 00:06:51 +0900

    Add files via upload

---
+++ 54.py
@@ -0,0 +1,16 @@
+a=int(input('Please enter the wavelength of visible light(The unit is nm.): '))
+
+if 380<=a<450:
+    print('The color of this visible light is violet.')
+elif 450<=a<495:
+    print('The color of this visible light is blue.')
+elif 495<=a<570:
+    print('The color of this visible light is green.')
+elif 570<=a<590:
+    print('The color of this visible light is yellow.')
+elif 590<=a<620:
+    print('The color of this visible light is orange.')
+elif 620<=a<750:
+    print('The color of this visible light is red.')
+else:
+    print('The wavelength of visible light ranges from 380 to 750nm.')
