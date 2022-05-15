3582de57a0c15609b29f6784c2edf99ea922e9d7
Jinny268 <101381590+Jinny268@users.noreply.github.com>
2022-05-16 01:14:16 +0900

    Add files via upload

---
+++ 56.py
@@ -0,0 +1,26 @@
+m=int(input('Please enter the minutes: '))
+t=int(input('Please enter the text: '))
+
+p0=(15+0.44)*1.05
+p1=(t-50)*0.15
+p2=(m-50)*0.25
+
+if m<=50:
+    if t<=50:
+        print('Base rate (including 911 fees and taxes): %.2f $.' %p0)
+        print('Total Amount: %.2f $' %p0)
+    elif t>50:
+        print('Base rate (including 911 fees and taxes): %.2f $.' %p0)
+        print('Additional Text Charges: %.2f $' %p1)
+        print('Total Amount: %.2f $' %(p0+p1))
+
+elif m>50:
+    if t<=50:
+        print('Base rate (including 911 fees and taxes): %.2f $.' %p0)
+        print('Additional Minutes Charge: %.2f $' %p2)
+        print('Total Amount: %.2f $' %(p0+p2))
+    elif t>50:
+        print('Base rate (including 911 fees and taxes): %.2f $.' %p0)
+        print('Additional Minutes Charge: %.2f $' %p2)
+        print('Additional Text Charges: %.2f $' %p1)
+        print('Total Amount: %.2f $' %(p0+p1+p2))
