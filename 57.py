781b73c5bc71e1925348ce9806fc055e3c4f3ed1
Jinny268 <101381590+Jinny268@users.noreply.github.com>
2022-05-16 01:16:16 +0900

    Add files via upload

---
+++ 57.py
@@ -0,0 +1,12 @@
+year=int(input('년도를 입력하세요(숫자만 입력): '))
+
+if year%400==0:
+    print('%d년은 윤년입니다.' %year)
+else:
+    if year%4==0:
+        if year%100==0:
+            print('%d년은 윤년이 아닙니다.' %year)
+        else:
+            print('%d년은 윤년입니다.' %year)
+    else:
+        print('%d년은 윤년이 아닙니다.' %year)
