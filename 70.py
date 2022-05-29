74d1822bfb46970fe0f62b1ec29e68aa4d07b65e
Jinny268 <101381590+Jinny268@users.noreply.github.com>
2022-05-29 10:09:25 +0900

    Add files via upload

---
+++ 70.py
@@ -0,0 +1,18 @@
+m=input('Please enter a message: ')
+s=int(input('Please enter the shift value: '))
+
+n_m=''
+for ch in m:
+    if ch>='a' and ch<='z':
+        p=ord(ch)-ord('a')
+        p=(p+s)%26
+        n_c=chr(p+ord('a'))
+        n_m=n_m+n_c
+    elif ch>='A' and ch <='Z':
+        p=ord(ch)-ord('A')
+        n_c=chr(p+ord('A'))
+        n_m=n_m+n_c
+    else:
+        n_m=n_m+ch
+
+print("This shifted message is '%s'." %n_m)
