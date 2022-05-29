661c029cafcab00ecb404801261878f88d1e738c
Jinny268 <101381590+Jinny268@users.noreply.github.com>
2022-05-30 00:00:59 +0900

    Add files via upload

---
+++ 72.py
@@ -0,0 +1,12 @@
+line=input('Please enter a string: ')
+
+i_p=True
+
+for i in range(0, len(line)//2):
+    if line[i]!=line[len(line)-i-1]:
+        i_p=False
+
+if i_p:
+    print('%s is a palindrome.' %line)
+else:
+    print('%s is not a palindrome' %line)
