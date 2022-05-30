cd39e3bd94c94b9904d5888dde2245a1aa13f79e
Jinny268 <101381590+Jinny268@users.noreply.github.com>
2022-05-31 00:03:08 +0900

    Add files via upload

---
+++ 78.py
@@ -0,0 +1,17 @@
+dec=int(input('Enter the decimal number you want to change to binary: '))
+
+bi=''
+q=dec
+
+while True:
+    if q==1 or q==0:
+        bi+=str(q)
+        break
+    r=q%2
+    bi+=str(r)
+    q//=2
+
+bi=list(bi)
+bi.reverse()
+bi=''.join(bi)
+print('%d is a binary number of %s.'%(dec, bi))
