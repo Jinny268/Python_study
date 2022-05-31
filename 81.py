b7fc0ee8be69505a614a5cb53c18cbe6a0c22197
Jinny268 <101381590+Jinny268@users.noreply.github.com>
2022-06-01 00:08:07 +0900

    Add files via upload

---
+++ 81.py
@@ -0,0 +1,4 @@
+def pythagoras(a, b):
+    print('Please enter the length of the hypotenuse: %d' %((a**2+b**2)**0.5))
+a, b=input('Please enter two shorter sides of the triangle: ').split()
+pythagoras(int(a), int(b))
