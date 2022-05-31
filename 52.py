ea26274860f7a4593582829bbbaa959c303e4560
Jinny268 <101381590+Jinny268@users.noreply.github.com>
2022-06-01 00:10:52 +0900

    Update 52.py

--- 52.py
+++ 52.py
@@ -1,3 +1,4 @@
+# 피타고라스 정리
 def pythagoras(a, b):
     print('Please enter the length of the hypotenuse: %d' %((a**2+b**2)**0.5))
 a, b=input('Please enter two shorter sides of the triangle: ').split()
