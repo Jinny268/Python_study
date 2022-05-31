ae9dcbd2b074343cd932f3f03905d43b6af8119a
Jinny268 <101381590+Jinny268@users.noreply.github.com>
2022-06-01 00:14:21 +0900

    Update 53.py

--- 53.py
+++ 53.py
@@ -1,3 +1,4 @@
+# 1부터 12 중에서 입력 받은 수를 서수로 출력하기
 def ordinal(a):
     if 1<=a<=12:
         ord={1: 'First', 2: 'Second', 3: 'Third', 4: 'Fourth', 5: 'Fifth', 6: 'Sixth', 7: 'Seventh', 8: 'Eighth', 9: 'Ninth', 10: 'Tenth', 11: 'Eleventh', 12: 'Twelfth'}
