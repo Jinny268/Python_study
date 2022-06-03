bb02e57e51c9477c8ea7998ecc0c985f68a4f7f0
Jinny268 <101381590+Jinny268@users.noreply.github.com>
2022-06-04 00:03:46 +0900

    Add files via upload

---
+++ 106.py
@@ -0,0 +1,27 @@
+def rem(data, num_1):
+    retval=sorted(data)
+
+    for i in range(num_1):
+        retval.pop()
+        
+    for i in range(num_1):
+        retval.pop(0)
+
+    return retval
+
+def main():
+    cards = []
+    c=input('Please enter a value (enter an empty line to exit): ')
+    
+    while c != '':
+        num=float(c)
+        cards.append(num)
+        c=input('Please enter a value (enter an empty line to exit): ')
+
+    if len(cards)<4:
+        print('Please enter at least 4 values.')
+    else:
+        print('List from which outliers have been removed: ', rem(cards, 2))
+        print('The original data: ', cards)
+
+main()
