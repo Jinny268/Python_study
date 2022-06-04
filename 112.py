97d46bd21be7665476f515f8b66666d6a6c6b948
Jinny268 <101381590+Jinny268@users.noreply.github.com>
2022-06-05 00:38:50 +0900

    Add files via upload

---
+++ 112.py
@@ -0,0 +1,12 @@
+a=[]
+
+num=input('Please enter an number(enter an empty line to exit): ')
+while num != '':
+    a.append(num)
+    num=input('Please enter an number(enter an empty line to exit): ')
+
+a=[int(i) for i in a]
+
+avg=sum(a)/len(a)
+print('평균: ', avg)
+print('리스트 값 들: ', int(a))
